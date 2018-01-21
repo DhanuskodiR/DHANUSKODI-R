0c6dee1f48f10b58b6a1dedf66c426df9abf041b
DhanuskodiR <35652433+DhanuskodiR@users.noreply.github.com>
2018-01-21 11:27:51 +0000

    Create vowel pos.c

---
+++ vowel pos.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main()
+{
+int a;
+printf("enter the no. a");
+scanf("%d",&a);
+if(a=='a'|| a=='e'|| a=='i'|| a=='o'|| a=='u')
+{
+printf("the no. is vowel");
+}
+else
+{printf("the no. is not vowel");
+}
+}
