1d5eeca8d0d37873da533d878e3c93a7eddb858e
DhanuskodiR <35652433+DhanuskodiR@users.noreply.github.com>
2018-01-23 16:18:40 +0530

    Create vowel1.C

---
+++ vowel1.C
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+char a;
+printf("enter the no.a");
+scanf("%c",&a);
+if(a=='a'||a=='e'||a=='i'||a=='o'||a=='u')
+{
+printf("the no.is vowel");
+}
+else
+{
+printf("the no.is not vowel");
+}
+}
