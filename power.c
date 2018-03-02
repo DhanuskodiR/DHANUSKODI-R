3d186a713fd2931f43fe48d1b5ddb9d3a63c4911
DhanuskodiR <35652433+DhanuskodiR@users.noreply.github.com>
2018-03-02 15:15:09 +0530

    Create power.c

---
+++ power.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<math.h>
+void main()
+{
+int  a,b,c;
+scanf("%d",&a);
+b=sqrt(a);
+c=pow(b,2);
+if(c==a)
+{
+printf("\n %d is power of 2",a);
+}
+else
+{
+    printf("\n %d is not power of 2",a);
+}
+}
