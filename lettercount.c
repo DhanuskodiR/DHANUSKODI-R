1e49a9376e7dcd2bc8d888c4777de0e9dd93f14f
DhanuskodiR <35652433+DhanuskodiR@users.noreply.github.com>
2018-03-13 10:52:49 +0000

    Create lettercount.c

---
+++ lettercount.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<string.h>
+
+void main()  
+{
+    char a[500];
+    int count=0,i;
+    scanf("%[^\n]s",&a);
+    for(i=0;a[i]!='\0';i++)
+    {
+        if(a[i]!=' ')
+        {
+            count++;
+        }
+    }
+    printf("count %d",count);
+}
