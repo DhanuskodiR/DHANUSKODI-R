229feda30ae2a9c28a4163878f3445e76e9f30ec
DhanuskodiR <35652433+DhanuskodiR@users.noreply.github.com>
2018-03-13 11:17:30 +0000

    Create sprcial char.c

---
+++ sprcial char.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<string.h>
+
+void main()  
+{
+    char a[500];
+    int count=0,i;
+ scanf("%[^\n]s",a);
+    for(i=0;a[i]!='\0';i++)
+    {
+        if(a[i]>='0' && a[i]<='9'|| a[i]>='a' && a[i]<='z')
+        {
+            continue;
+        }
+        else
+        {
+        ++count;
+        }
+    }
+    printf("count %d",count);
+}
