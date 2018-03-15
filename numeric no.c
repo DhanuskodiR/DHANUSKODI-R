5176d5215df7ae8bfc01d6f496cddf0e7e2d97c3
DhanuskodiR <35652433+DhanuskodiR@users.noreply.github.com>
2018-03-15 10:35:07 +0000

    Create numeric no.c

---
+++ numeric no.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+int num;
+printf("enter the numeric values:",num);
+scanf("%d",&num);
+if(num != 'a'||'z')
+{
+  printf("yes,it is numeric");
+}
+else
+{
+printf("no,it is not numeric");
+}
+}
